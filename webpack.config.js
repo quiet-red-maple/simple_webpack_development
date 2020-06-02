c52fe401a08055d557fe948b0b4863fbfaffaae4
郝泉宇 <13121800163@163.com>
2020-06-02 14:53:21 +0800

    开发环境webpack搭建

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+// 开发环境配置
+// 运行指令 npx webpack-dev-server
+// 
+
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'js/built.js',
+    path: resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 8 * 1024,
+          name: '[hash:10].[ext]',
+          outputPath: 'imgs'
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+        exclude: /\.(html|js|css|less|jpg|png|jpeg|png|gif)/,
+        loader: 'file-loader',
+        options: {
+          name: '[hash:10].[ext]',
+          outputPath: 'media'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/html/index.html'
+    })
+  ],
+  mode: 'development',
+  devServer: {
+    contentBase: resolve(__dirname, 'build'),
+    compress: true,
+    port: 3000,
+    open: true
+  }
+}
